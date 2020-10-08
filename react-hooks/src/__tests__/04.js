4b4273e99fc6fd7b79ed80fd7afa8fd4d589cfb5
AlexMachin1997 <AlexMachin1997@gmail.com>
2020-10-08 09:39:36 +0100

    Added the history function (Thanks for the walk through Kent) and added additional comments for future reference

--- react-hooks/src/__tests__/04.js
+++ react-hooks/src/__tests__/04.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-// import App from '../final/04'
-import App from '../exercise/04'
+import App from '../final/04'
+// import App from '../exercise/04'
 
 test('can play a game of tic tac toe', () => {
   const {container} = render(<App />)
--- react-hooks/src/exercise/04.js
+++ react-hooks/src/exercise/04.js
@@ -5,6 +5,187 @@ import React from 'react'
 
 import {useLocalStorageState} from '../utils';
 
+// Generates an array with null values, these slowly get replaced with X's or O's
+const initialSquares = Array(9).fill(null);
+
+// Board component - Handles the board interactions ie click events for the squares, generating the 3x3 grid
+const Board = ({squares, onClick}) => {
+
+  // Generates a single square
+  const renderSquare = i => (
+      <button className="square" onClick={() => onClick(i)}>
+        {squares[i]}
+      </button>
+    );
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`
+}
+
+function calculateNextValue(squares) {
+
+  // Get the length of all the squares which have a value of X 
+  const xSquaresCount = squares.filter(r => r === 'X').length
+
+  // Get the length of all the squares which have O
+  const oSquaresCount = squares.filter(r => r === 'O').length
+
+  // Switch between X and O's. Each time an item is added to the array the item will be flipped. 
+  // 0 === 0 -> true (X) , 0 === 1 -> false (O), 1 === 1 -> true (X)
+  return oSquaresCount === xSquaresCount ? 'X' : 'O'
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  // Loop through the lines array
+  for (let i = 0; i < lines.length; i++) {
+
+    // Destructuring the elements from the array e.g. [0, 1, 2] becomes individual variables e.g. a, b, c
+    const [a, b, c] = lines[i]
+
+    // Checks to see if the current element has three in a row 
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
+}
+
+const Game = () => {
+
+  // Gets set when you either select a square, select a record from the history or if you reset the game.
+  const [currentStep, setCurrentStep] = useLocalStorageState('tic-tac-toe:step', 0);
+
+   // Gets set when you either select a square, select a record from the history or if you reset the game.
+   // This stores a multi-dimensional array e.g. [[null, null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null, "X"]]
+  const [history, setHistory] = useLocalStorageState('tic-tac-toe:history', [initialSquares]);
+
+  // Accesses an element of the history via the currentStep e.g. step 1 returns the second move you made
+  const currentSquares = history[currentStep];
+  
+  // Calculate the next value e.g. X or O
+  const nextValue  = calculateNextValue(currentSquares);
+
+  // Calculate the winner (Not too sure how this works exactly)
+  const winner = calculateWinner(currentSquares);
+
+  // Calculate the games status (Returns the next player or the winner of the game)
+  const status = calculateStatus(winner, currentSquares, nextValue);
+
+
+  function selectSquare(index) {
+
+    // winner === true || Square index is already taken then don't allow allow the current square to be updated (Square taken -> true, else false)
+    if(winner ||  currentSquares[index]) return;
+
+    // Take a copy of the history array
+    const newHistory = history.slice(0, currentStep + 1);
+
+    // Copy the state, it can't or at least shouldn't be modified directly. So copy it then reset the state
+    const squaresCopy = [...currentSquares];
+
+    // Update the selected square with the next value e.g. X or O
+    squaresCopy[index] = nextValue;
+
+    // Spread the new history (Array with modified element) and add the existing array elements to the history (Don't delete the old history)
+    setHistory([...newHistory, squaresCopy])
+    
+    // The current step is the number of elements inside of the newHistory e.g. second click is step 2 for example
+    setCurrentStep(newHistory.length)
+  }
+
+  // Generates a list for each history item
+  const moves = history.map((_, index) => {
+  
+    // When the index is 0 show "Go to game start", else "Go to move #{index}"
+    const desc = index === 0 ? 'Go to game start' : `Go to move #${index}`;
+    
+    // If the index is the same as the current step then it's the current Index
+    // This disabled the button and appends "(Current)" to the description
+    const isCurrentStep = index === currentStep;
+
+    // Add the UL to the moves array e.g. <li> Go to move #2 (Current) </li>
+    // Disable the UI if it's the current move e.g. you selected move 5 you shouldn't be able to click it again.
+    return (
+      <li key={index}>
+        <button disabled={isCurrentStep} onClick={() => setCurrentStep(index)}>
+          {desc} {isCurrentStep ? '(Current)' : null}
+        </button>
+      </li>
+    )
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={() => {
+          setHistory([initialSquares]);
+          setCurrentStep(0);
+        }}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+const App = () => <Game/>
+
+export default App
+
+
+/* 
+
+The version which works with 04-extra-1.js
+
+// useState: tic tac toe
+// http://localhost:3000/isolated/exercise/04.js
+
+import React from 'react'
+
+import {useLocalStorageState} from '../utils';
+
 const initialSquares = Array(9).fill(null);
 
 const Board = () => {
@@ -136,3 +317,5 @@ const Game = () => (
 const App = () => <Game/>
 
 export default App
+
+*/
