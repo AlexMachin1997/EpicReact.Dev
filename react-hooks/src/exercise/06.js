b7427ed1fdcb0b14379334452ec4d57e08413149
AlexMachin1997 <AlexMachin1997@gmail.com>
2020-10-10 17:47:09 +0100

    Updated exercise 6, the useEffect is now it's own custom hook.

--- react-hooks/src/exercise/06.js
+++ react-hooks/src/exercise/06.js
@@ -2,22 +2,16 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import React, { useEffect, useState } from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {fetchPokemon, PokemonDataView, PokemonForm, PokemonInfoFallback} from '../pokemon'
 
 
 /* 
-
 -------------------------------------------------------------------------------------
 Core Task:
 -------------------------------------------------------------------------------------
 
 In this exercise, we'll be doing data fetching directly in a useEffect hook
 callback within our component.
-
 Here we have a form where users can enter the name of a pokemon and fetch data
 about that pokemon. Your job will be to create a component which makes that
 fetch request. When the user submits a pokemon name, our `PokemonInfo` component
@@ -30,7 +24,6 @@ Extra Credits:
 - Handling errors
 
 Sometimes requests will fail for various reasons, for example not finding a pokemon or not connecting to an API
-
 To let the user know the application was unable to find some pokemon's an error will need to be shown, store this message in state
 
 -------------------------------------------------------------------------------------
@@ -41,7 +34,6 @@ The error handling is messy and very difficult to follow, to make it easier to f
 store the network status.
 
 Examples of status:
-
 idle: no request made yet
 pending: request started
 resolved: request successful
@@ -54,20 +46,17 @@ NOTE: Don't use an if, else if, else approach. Use a switch as it's based on one
 - Storing object in state (IMPORTANT)
 
 Currently each piece of state is stored individually ie multiple [] = useState() are declared.
-
 Currently this isn't an issue, but if for some reason the state needed to be set in a different order then this would become an issue.
 If the status was set to resolved and the pokemon data wasn't set then the component would throw an error e.g. image property not found.
 
 Example:
 
 Was:
-
 const [pokemon, setPokemon] = useState(null);
 const [status, setStatus] = useState('idle');
 const [error, setError] = useState(null);
 
 Now:
-
 const [state, setState] = useState({
   pokemon: null,
   status: 'idle',
@@ -79,9 +68,10 @@ NOTE:
 When the state is set all the previous properties won't be available. We could use the spread operator and
 create a custom component, but it's not needed. Plus later on we will use the useReducer hook which handles this type of state ie object based
 
+-------------------------------------------------------------------------------------
 */
 
-function PokemonInfo({pokemonName}) {
+const usePokemon = (pokemonName) => {
 
   // The state is stored inside of one object, this prevents multiple re-renders from occurring and the setting of the state doesn't matter now.
   // NOTE: When the state is set the existing properties will be removed, we could spread them but there is no need in this case. Plus there is a better option later on, useReducer hook
@@ -103,14 +93,13 @@ function PokemonInfo({pokemonName}) {
     /* 
     
     useEffect note:
+    
+    - useEffect can't be asynchronous because the useEffect hook returns a cleanup function and not a promise
 
-    useEffect can't be asynchronous because the useEffect hook returns a cleanup function and not a promise
-
-    To overcome this we create an inline function and execute in the useEffect (We don't need to await the function in this case)
-
-
+    - To overcome the async problem a custom async hook can be created and then called below (We don't need to await the function in this case)
+    
     Resources:
-
+    
     - https://medium.com/javascript-in-plain-english/how-to-use-async-function-in-react-hook-useeffect-typescript-js-6204a788a435
 
     */
@@ -133,12 +122,21 @@ function PokemonInfo({pokemonName}) {
       }      
     }
 
-    // Call the inline function
+    // Call the inline function (No need to await it in this case)
     getAPokemon();
 
   }, [pokemonName])
 
+  // Return the values - The order matters when destructuring the values whilst using the function
+  return [pokemon, error, status];
+}
 
+function PokemonInfo({pokemonName}) {
+
+  // Must be destructured in the same as the values are returned, they aren't 
+  const [pokemon, error, status] = usePokemon(pokemonName);
+
+  // Render the content based on the status of the request
   switch(status) {
     case 'idle': {
       return 'Submit a Pokemon Name';
@@ -166,12 +164,10 @@ function PokemonInfo({pokemonName}) {
   }
 }
 
-function App() {
+const App = () => {
   const [pokemonName, setPokemonName] = React.useState('')
 
-  function handleSubmit(newPokemonName) {
-    setPokemonName(newPokemonName)
-  }
+  const handleSubmit = newPokemonName => setPokemonName(newPokemonName)
 
   return (
     <div className="pokemon-info-app">
@@ -179,7 +175,7 @@ function App() {
       <hr />
       <div className="pokemon-info">
         <PokemonInfo pokemonName={pokemonName} />
-      </div>
+      </div>    
     </div>
   )
 }
